cad64d48eeb7c84266620faef1f7956c051cbf59
Indu Chandana <indu.99.ic@gmail.com>
2023-10-05 20:29:41 +0530

    Oct5 2023 Happy coding

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  return (
+    <div
+      className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100"
+    >
+
+    </div>
+  )
+}
