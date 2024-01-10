e8789c01e89a8b05faafa85330a82d3cdee2b532
Indu Chandana <indu.99.ic@gmail.com>
2024-01-10 22:36:39 +0530

    Jan10 001 start creating chat messaging

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 import { useChat } from 'ai/react' // use for streaming text UI
 
 import { Input } from './ui/input'
+import { Send } from 'lucide-react'
+import { Button } from './ui/button'
+import MessageList from './MessageList'
 
 type Props = {}
 
 const ChatComponent = (props: Props) => {
-    const { input, handleInputChange, handleSubmit } = useChat();
+    const { input, handleInputChange, handleSubmit, messages } = useChat();
 
     return (
         <div className=' relative max-h-screen overflow-scroll scrollbar-hide'>
@@ -17,9 +20,13 @@ const ChatComponent = (props: Props) => {
             </div>
 
             {/* message list */}
+            <MessageList messages={messages} />
 
             <form onSubmit={handleSubmit} className=' sticky bottom-0 inset-x-0 px-2 py-4 bg-white'>
-                <Input value={input} onChange={handleInputChange} placeholder='Ask any question...' className=' w-full' />
+                <div className='flex'>
+                    <Input value={input} onChange={handleInputChange} placeholder='Ask any question...' className=' w-full' />
+                    <Button className=" bg-blue-600 ml-2"><Send className='h-4 w-4' /></Button>
+                </div>
             </form>
         </div>
     )
