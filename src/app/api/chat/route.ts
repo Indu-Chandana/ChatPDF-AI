c60a594eee5514efa19496fd312bce8b051d35ac
Indu Chandana <indu.99.ic@gmail.com>
2024-01-13 16:12:56 +0530

    Jan13 001 add instructions in chatbot logic via comments

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -12,16 +12,21 @@ export async function POST(req: Request) {
     try {
         const { messages } = await req.json()
 
+        // I send - Hello
+        // res(bot) - Hello, How can I help you.
+
         // Ask OpenAI for a streaming chat completion given the prompt
         const response = await openai.createChatCompletion({
             model: 'gpt-3.5-turbo', // very fast model
             messages,
-            stream: true // animation like chat bot
+            stream: true // animation like chat bot - send req to OpenAI instead waiting for generate whole res,  it send me chunk by chunck 
         })
 
         // Ask OpenAI for a streaming chat completion given the prompt
         const stream = OpenAIStream(response)
         // Respond with the stream
+
+        // I think - we are using |useChat()| for our form.  therefore we need to return back our res friendly with 'ai' library (vercel ai sdk)
         return new StreamingTextResponse(stream)
     } catch (error) {
         console.warn('error - /api/chat POST method ::', error)
