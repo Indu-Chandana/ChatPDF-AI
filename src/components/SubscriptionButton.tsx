0e456daa1df587445df80b141e26a9006575cf10
Indu Chandana <indu.99.ic@gmail.com>
2024-02-02 19:44:21 +0530

    Feb02 001 change subscription btn variant

--- src/components/SubscriptionButton.tsx
+++ src/components/SubscriptionButton.tsx
@@ -22,7 +22,7 @@ const SubscriptionButton = (props: Props) => {
     }
 
     return (
-        <Button disabled={loading} onClick={handleSubscription}>
+        <Button disabled={loading} onClick={handleSubscription} variant="secondary">
             {props.isPro ? "Manage Subscriptions" : "Get Pro"}
         </Button>
     )
